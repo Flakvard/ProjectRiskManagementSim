9bc1d2933904c6d462419991d6abaff067060867
Marni <marni@falkvard.com>
2024-09-17 12:03:44 +0200

    Rename project file

--- ProjectRiskManagementSim.ProjectSimulator/ProjectSimulationModel.cs
+++ ProjectRiskManagementSim.ProjectSimulator/ProjectSimulationModel.cs
@@ -1,14 +1,15 @@
+namespace ProjectRiskManagementSim.ProjectSimulation;
 
-public class ProjectSimulationModel
+internal class ProjectSimulationModel
 {
     public List<StaffModel> Staff { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime TargetDate { get; set; }
     public RevenueModel Revenue { get; set; }
-    public CostModel Costs { get; set; }
-    public List<DeliverableModel> Deliverables { get; set; }
-    public List<PhaseModel> Phases { get; set; }
-    public List<ColumnModel> Columns { get; set; }
-    public List<DefectModel> Defects { get; set; }
-    public List<BlockingEventModel> BlockingEvents { get; set; }
+    // public CostModel Costs { get; set; }
+    // public List<DeliverableModel> Deliverables { get; set; }
+    // public List<PhaseModel> Phases { get; set; }
+    // public List<ColumnModel> Columns { get; set; }
+    // public List<DefectModel> Defects { get; set; }
+    // public List<BlockingEventModel> BlockingEvents { get; set; }
 }
