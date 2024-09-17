cc7b81d282b0c28b5294a24edeaf2fbd7e9acd5e
Marni <marni@falkvard.com>
2024-09-17 12:48:50 +0200

    added revenue pr day

--- ProjectRiskManagementSim.ProjectSimulator/MonteCarloSimulation.cs
+++ ProjectRiskManagementSim.ProjectSimulator/MonteCarloSimulation.cs
@@ -16,24 +16,30 @@ internal class MonteCarloSimulation
         var startDate = _projectSimulationModel.StartDate;
         var targetDate = _projectSimulationModel.TargetDate;
         var revenue = _projectSimulationModel.Revenue;
+        var cost = _projectSimulationModel.Costs;
 
-        if (staff == null || revenue == null)
+        if (staff == null || revenue == null || cost == null)
         {
-            throw new InvalidOperationException("Staff and Revenue must be set before running simulation");
+            throw new InvalidOperationException("Staff, Revenue, and Cost must be set before running the simulation.");
         }
 
         var days = (targetDate - startDate).TotalDays;
-        var revenuePerDay = revenue.Amount / days;
 
         var totalRevenue = 0.0;
+        var totalCost = 0.0;
         for (var i = 0; i < staff.Count; i++)
         {
             var staffMember = staff[i];
             var staffDays = staffMember.Days;
-            var staffRevenue = staffDays * revenuePerDay;
+            var staffRevenue = staffMember.Sale * staffDays;
+            var staffCost = staffMember.Cost * staffDays;
             totalRevenue += staffRevenue;
+            totalCost += staffCost;
         }
+        var revenuePerDay = revenue.Amount / days;
+        var costPerDay = cost.Cost / days;
 
         Console.WriteLine($"Total Revenue: {totalRevenue}");
+        Console.WriteLine($"Total Cost: {totalCost}");
     }
 }
--- ProjectRiskManagementSim.ProjectSimulator/Program.cs
+++ ProjectRiskManagementSim.ProjectSimulator/Program.cs
@@ -7,15 +7,16 @@ var projectSimModel = new ProjectSimulationModel
 {
     Staff = new List<StaffModel>
     {
-      new StaffModel { Name = "John Doe", Role = Role.ProjectManager, Cost = 1000, Days = 20 },
-      new StaffModel { Name = "Jane Doe", Role = Role.FrontendDeveloper, Cost = 800, Days = 20 },
-      new StaffModel { Name = "Jack Doe", Role = Role.BackendDeveloper, Cost = 800, Days = 20 },
-      new StaffModel { Name = "Jill Doe", Role = Role.SoftwareTester, Cost = 800, Days = 20 },
-      new StaffModel { Name = "Jim Doe", Role = Role.UXUIDesigner, Cost = 800, Days = 20 }
+      new StaffModel { Name = "John Doe", Role = Role.ProjectManager, Sale = 1000, Cost = 370, Days = 20 },
+      new StaffModel { Name = "Jane Doe", Role = Role.FrontendDeveloper, Sale = 1000, Cost = 370, Days = 20 },
+      new StaffModel { Name = "Jack Doe", Role = Role.BackendDeveloper, Sale = 1000, Cost = 370, Days = 20 },
+      new StaffModel { Name = "Jill Doe", Role = Role.SoftwareTester, Sale = 1000, Cost = 370, Days = 20 },
+      new StaffModel { Name = "Jim Doe", Role = Role.UXUIDesigner, Sale = 1000, Cost = 370, Days = 20 }
     },
     StartDate = new DateTime(2024, 1, 1),
     TargetDate = new DateTime(2024, 8, 31),
-    Revenue = new RevenueModel { Amount = 1000 }
+    Revenue = new RevenueModel { Amount = 1000 },
+    Costs = new CostModel { Cost = 370, Days = 20 }
 };
 
 var MCS = new MonteCarloSimulation(projectSimModel);
