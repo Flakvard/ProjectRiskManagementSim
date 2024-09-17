7655d600d6a93ed8e7ea14289fb2da68898f5ab9
Marni <marni@falkvard.com>
2024-09-17 10:38:54 +0200

    Renamed folder and added ProjectSimModel

---
+++ ProjectRiskManagementSim.ProjectSimulator/ProjectSimulationModel.cs
@@ -0,0 +1,14 @@
+
+public class ProjectSimulationModel
+{
+    public List<StaffModel> Staff { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime TargetDate { get; set; }
+    public RevenueModel Revenue { get; set; }
+    public CostModel Costs { get; set; }
+    public List<DeliverableModel> Deliverables { get; set; }
+    public List<PhaseModel> Phases { get; set; }
+    public List<ColumnModel> Columns { get; set; }
+    public List<DefectModel> Defects { get; set; }
+    public List<BlockingEventModel> BlockingEvents { get; set; }
+}
