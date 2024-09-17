a35f132a6739995617b923321fc226b27bc3911e
Marni <marni@falkvard.com>
2024-09-17 12:46:51 +0200

    Added cost model

---
+++ ProjectRiskManagementSim.ProjectSimulator/CostModel.cs
@@ -0,0 +1,7 @@
+namespace ProjectRiskManagementSim.ProjectSimulation;
+
+internal class CostModel
+{
+    public double Cost { get; set; }
+    public double Days { get; set; }
+}
---
+++ ProjectRiskManagementSim.UnitTest/ProjectRiskManagementSim.ProjectSimulator/CostModelTest.cs
@@ -0,0 +1,26 @@
+using ProjectRiskManagementSim.ProjectSimulation;
+
+namespace ProjectRiskManagementSim.UnitTest;
+
+public class CostModelTest
+{
+    [Fact]
+    public void CostModel()
+    {
+        // Arrange
+        var Cost = new CostModel
+        {
+            Cost = 1000,
+            Days = 20,
+        };
+
+        // Act
+        var cost = Cost.Cost;
+        var days = Cost.Days;
+
+        // Assert
+        Assert.Equal(1000, cost);
+        Assert.Equal(20, days);
+    }
+}
+
