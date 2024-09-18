a8d82c4292b3643a3512f504db885b66191b75de
Marni <marni@falkvard.com>
2024-09-18 12:31:15 +0200

    Added default constructor and a non-nullable compare method

--- ProjectRiskManagementSim.ProjectSimulator/DeliverableModel.cs
+++ ProjectRiskManagementSim.ProjectSimulator/DeliverableModel.cs
@@ -5,9 +5,23 @@ internal class DeliverableModel : IComparer<DeliverableModel>
     public Guid Id { get; set; }
     public int Nr { get; set; }
     public double CompletionDays { get; set; }
+    public double AccumulatedDays { get; set; }
 
-    public int Compare(DeliverableModel x, DeliverableModel y)
+    public DeliverableModel()
     {
+        Id = Guid.NewGuid();
+        Nr = 0;
+        CompletionDays = 0;
+        AccumulatedDays = 0;
+    }
+    public int Compare(DeliverableModel? x, DeliverableModel? y)
+    {
+        if (x == null && y == null) return 0;   // Both are null, considered equal
+        if (x == null) return -1;               // Null is considered "less than" a non-null value
+        if (y == null) return 1;                // Non-null is considered "greater than" null
+
+        // Perform comparison based on Nr property
         return x.Nr.CompareTo(y.Nr);
     }
+
 }
