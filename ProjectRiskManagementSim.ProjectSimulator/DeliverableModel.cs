b2025f0d64a5d008789168efbaae5ef93a0076fb
Marni <marni@falkvard.com>
2024-09-18 09:06:27 +0200

    Added Dumpify and Compare method to Deliverables

--- ProjectRiskManagementSim.ProjectSimulator/DeliverableModel.cs
+++ ProjectRiskManagementSim.ProjectSimulator/DeliverableModel.cs
@@ -1,8 +1,13 @@
 namespace ProjectRiskManagementSim.ProjectSimulation;
 
-internal class DeliverableModel
+internal class DeliverableModel : IComparer<DeliverableModel>
 {
     public Guid Id { get; set; }
     public int Nr { get; set; }
     public double CompletionDays { get; set; }
+
+    public int Compare(DeliverableModel x, DeliverableModel y)
+    {
+        return x.Nr.CompareTo(y.Nr);
+    }
 }
