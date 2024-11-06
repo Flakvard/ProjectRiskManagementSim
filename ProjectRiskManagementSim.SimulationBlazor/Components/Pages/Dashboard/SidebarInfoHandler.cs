15c08b386f91b68e79fc9552fe1d218aa7abe679
Ma-a-a-rni <marnifalkvard@gmail.com>
2024-11-06 11:33:43 +0100

    Injecting SimualtionId to Dashbord and get info from db

---
+++ ProjectRiskManagementSim.SimulationBlazor/Components/Pages/Dashboard/SidebarInfoHandler.cs
@@ -0,0 +1,33 @@
+using ProjectRiskManagementSim.DataAccess;
+using ProjectRiskManagementSim.DataAccess.Models;
+namespace ProjectRiskManagementSim.SimulationBlazor.Components.Pages.Dashboard;
+public class SidebarInfoHandler
+{
+    public ProjectModel? _project { get; set; }
+    public List<ProjectSimulationModel> _simulations { get; set; } = new List<ProjectSimulationModel>();
+    public ProjectSimulationModel? _simulation { get; set; }
+    public int SimulationId { get; set; }
+    public SidebarInfoHandler(int projectId)
+    {
+        SimulationId = projectId;
+    }
+    public async Task InitializeProjectsAsync(OxygenSimulationContext context)
+    {
+        // Initialize the list of simulations
+        var simulation = await context.GetSimulationByIdAsync(SimulationId);
+        if (simulation != null)
+        {
+            _simulation = simulation;
+            // Get the project for the simulation
+            var project = await context.GetProjectBySimulationIdAsync(simulation.ProjectId);
+            _project = project;
+            // Get all simulations for the project
+            if (project != null)
+            {
+                var simulations = await context.GetProjectSimulationsAsync(project.Id);
+                if (simulations != null)
+                    _simulations = simulations;
+            }
+        }
+    }
+}
