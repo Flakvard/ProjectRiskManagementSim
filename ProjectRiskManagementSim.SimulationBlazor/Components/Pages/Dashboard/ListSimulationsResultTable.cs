0a63c302677a09ce264ad837b6535625170d0c6b
Marni <marni@falkvard.com>
2024-11-12 15:58:15 +0100

    Added bug % SimCost to result table

--- ProjectRiskManagementSim.SimulationBlazor/Components/Pages/Dashboard/ListSimulationsResultTable.cs
+++ ProjectRiskManagementSim.SimulationBlazor/Components/Pages/Dashboard/ListSimulationsResultTable.cs
@@ -39,10 +39,12 @@ public partial class ListSimulationsResultTable
                 TargetDate = simulation.TargetDate.ToString("dd MMM yyyy"),
                 Days = (int)(simulation.SimulationDays ?? 0),
                 Budget = simulation.BudgetCosts?.ToString("N0") ?? "0",
+                SimCost = simulation.SimulationCosts?.ToString("N0") ?? "0",
                 ActualCost = simulation.ActualCosts?.ToString("N0") ?? "0",
-                CostPerDay = simulation.CostPrDay?.ToString() ?? "0",
+                CostPerDay = simulation.CostPrDay?.ToString("N0") ?? "0",
                 DelayPerDay = simulation.SimulationDaysOfDelay?.ToString() ?? "0",
-                Epics = (int)simulation.DeliverablesCount
+                Epics = (int)simulation.DeliverablesCount,
+                BugPercentage = ((simulation.BugPercentage / 100).ToString("P0")) ?? "0"
             });
         }
         return resultTableModels;
@@ -56,63 +58,34 @@ public class SimulationsResultTableModel
     public string TargetDate { get; set; }
     public int Days { get; set; }
     public string Budget { get; set; }
+    public string SimCost { get; set; }
     public string ActualCost { get; set; }
     public string CostPerDay { get; set; }
     public string DelayPerDay { get; set; }
     public int Epics { get; set; }
+    public string BugPercentage { get; set; }
 
     public static List<SimulationsResultTableModel> InitializeSimulationResults()
     {
-        return new List<SimulationsResultTableModel>{
-        new SimulationsResultTableModel
+        var list = new List<SimulationsResultTableModel>();
+        for (int i = 0; i < 4; i++)
         {
-          SimulationName = "None",
-          SimulationDate = "None",
-          TargetDate = "None",
-          Days = 0,
-          Budget = "0",
-          ActualCost = "0",
-          CostPerDay = "0",
-          DelayPerDay = "0",
-          Epics = 0
-        },
-        new SimulationsResultTableModel
-        {
-          SimulationName = "None",
-          SimulationDate = "None",
-          TargetDate = "None",
-          Days = 0,
-          Budget = "0",
-          ActualCost = "0",
-          CostPerDay = "0",
-          DelayPerDay = "0",
-          Epics = 0,
-        },
-        new SimulationsResultTableModel
-        {
-          SimulationName = "None",
-          SimulationDate = "None",
-          TargetDate = "None",
-          Days = 0,
-          Budget = "0",
-          ActualCost = "0",
-          CostPerDay = "0",
-          DelayPerDay = "0",
-          Epics = 0,
-        },
-        new SimulationsResultTableModel
-        {
-          SimulationName = "None",
-          SimulationDate = "None",
-          TargetDate = "None",
-          Days = 0,
-          Budget = "0",
-          ActualCost = "0",
-          CostPerDay = "0",
-          DelayPerDay = "0",
-          Epics = 0,
+            list.Add(new SimulationsResultTableModel
+            {
+                SimulationName = "None",
+                SimulationDate = "None",
+                TargetDate = "None",
+                Days = 0,
+                Budget = "0",
+                ActualCost = "0",
+                SimCost = "0",
+                CostPerDay = "0",
+                DelayPerDay = "0",
+                Epics = 0,
+                BugPercentage = "0"
+            });
         }
-      };
+        return list;
     }
 }
 
