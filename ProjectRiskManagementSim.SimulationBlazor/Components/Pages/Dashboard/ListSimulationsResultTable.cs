b393fddc5612c73a3e684eef96749f81ca22da4e
Marni <marni@falkvard.com>
2024-11-12 14:08:25 +0100

    Added simulations to simResultTable

--- ProjectRiskManagementSim.SimulationBlazor/Components/Pages/Dashboard/ListSimulationsResultTable.cs
+++ ProjectRiskManagementSim.SimulationBlazor/Components/Pages/Dashboard/ListSimulationsResultTable.cs
@@ -1,4 +1,18 @@
+using ProjectRiskManagementSim.DataAccess.Models;
+
 namespace ProjectRiskManagementSim.SimulationBlazor.Components.Pages.Dashboard;
+
+public partial class ListSimulationsResultTable
+{
+
+    public List<ProjectSimulationModel> Simulations { get; set; }
+    public List<SimulationsResultTableModel> SimulationsResultTableModels;
+
+    public ListSimulationsResultTable()
+    {
+        SimulationsResultTableModels = SimulationsResultTableModel.InitializeSimulationResults();
+    }
+}
 
 public class SimulationsResultTableModel
 {
@@ -6,10 +20,10 @@ public class SimulationsResultTableModel
     public string SimulationDate { get; set; }
     public string TargetDate { get; set; }
     public int Days { get; set; }
-    public decimal Budget { get; set; }
-    public decimal ActualCost { get; set; }
-    public decimal CostPerDay { get; set; }
-    public decimal DelayPerDay { get; set; }
+    public string Budget { get; set; }
+    public string ActualCost { get; set; }
+    public string CostPerDay { get; set; }
+    public string DelayPerDay { get; set; }
     public int Epics { get; set; }
 
     public static List<SimulationsResultTableModel> InitializeSimulationResults()
@@ -17,51 +31,51 @@ public class SimulationsResultTableModel
         return new List<SimulationsResultTableModel>{
         new SimulationsResultTableModel
         {
-          SimulationName = "Phase 1",
-          SimulationDate = "25.09.2024",
-          TargetDate = "01-10-2024",
-          Days = -6,
-          Budget = 1100000,
-          ActualCost = 80000,
-          CostPerDay = 2846,
-          DelayPerDay = 5320,
-          Epics = 25
-        },
-        new SimulationsResultTableModel
-        {
-          SimulationName = "Ekstra feature on project",
-          SimulationDate = "20.10.2024",
-          TargetDate = "01-10-2024",
-          Days = 19,
-          Budget = 1200000,
-          ActualCost = 110000,
-          CostPerDay = 2996,
-          DelayPerDay = 5320,
-          Epics = 30
-        },
-        new SimulationsResultTableModel
-        {
-          SimulationName = "New Deadline",
-          SimulationDate = "09.11.2024",
-          TargetDate = "09-11-2024",
+          SimulationName = "None",
+          SimulationDate = "None",
+          TargetDate = "None",
           Days = 0,
-          Budget = 1200000,
-          ActualCost = 140000,
-          CostPerDay = 3154,
-          DelayPerDay = 5320,
-          Epics = 30
+          Budget = "0",
+          ActualCost = "0",
+          CostPerDay = "0",
+          DelayPerDay = "0",
+          Epics = 0
         },
         new SimulationsResultTableModel
         {
-          SimulationName = "Phase II of bech bruun project",
-          SimulationDate = "19.11.2024",
-          TargetDate = "09-11-2024",
-          Days = 10,
-          Budget = 1250000,
-          ActualCost = 180000,
-          CostPerDay = 3154,
-          DelayPerDay = 5320,
-          Epics = 30
+          SimulationName = "None",
+          SimulationDate = "None",
+          TargetDate = "None",
+          Days = 0,
+          Budget = "0",
+          ActualCost = "0",
+          CostPerDay = "0",
+          DelayPerDay = "0",
+          Epics = 0,
+        },
+        new SimulationsResultTableModel
+        {
+          SimulationName = "None",
+          SimulationDate = "None",
+          TargetDate = "None",
+          Days = 0,
+          Budget = "0",
+          ActualCost = "0",
+          CostPerDay = "0",
+          DelayPerDay = "0",
+          Epics = 0,
+        },
+        new SimulationsResultTableModel
+        {
+          SimulationName = "None",
+          SimulationDate = "None",
+          TargetDate = "None",
+          Days = 0,
+          Budget = "0",
+          ActualCost = "0",
+          CostPerDay = "0",
+          DelayPerDay = "0",
+          Epics = 0,
         }
       };
     }
