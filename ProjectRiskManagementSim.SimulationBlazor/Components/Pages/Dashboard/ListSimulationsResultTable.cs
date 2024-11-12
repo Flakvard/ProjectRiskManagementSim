6a2f85ac478845ee406f79d4c74513315499ca3d
Marni <marni@falkvard.com>
2024-11-12 16:43:25 +0100

    Added anchor tags to table and a visual to which is selected

--- ProjectRiskManagementSim.SimulationBlazor/Components/Pages/Dashboard/ListSimulationsResultTable.cs
+++ ProjectRiskManagementSim.SimulationBlazor/Components/Pages/Dashboard/ListSimulationsResultTable.cs
@@ -32,8 +32,11 @@ public partial class ListSimulationsResultTable
         List<SimulationsResultTableModel> resultTableModels = new List<SimulationsResultTableModel>();
         foreach (var simulation in simulations)
         {
+            var costDifference = simulation.SimulationCosts - simulation.BudgetCosts;
+            var simCostPerDay = simulation.SimulationCosts / simulation.SimulationDays;
             resultTableModels.Add(new SimulationsResultTableModel
             {
+                Id = simulation.Id,
                 SimulationName = simulation.Name ?? "N/A",
                 SimulationDate = simulation.SimEndDate?.ToString("dd MMM yyyy") ?? "N/A",
                 TargetDate = simulation.TargetDate.ToString("dd MMM yyyy"),
@@ -41,10 +44,12 @@ public partial class ListSimulationsResultTable
                 Budget = simulation.BudgetCosts?.ToString("N0") ?? "0",
                 SimCost = simulation.SimulationCosts?.ToString("N0") ?? "0",
                 ActualCost = simulation.ActualCosts?.ToString("N0") ?? "0",
+                CostDifference = costDifference?.ToString("N0") ?? "0",
                 CostPerDay = simulation.CostPrDay?.ToString("N0") ?? "0",
+                SimCostPerDay = simCostPerDay?.ToString("N0") ?? "0",
                 DelayPerDay = simulation.SimulationDaysOfDelay?.ToString() ?? "0",
                 Epics = (int)simulation.DeliverablesCount,
-                BugPercentage = ((simulation.BugPercentage / 100).ToString("P0")) ?? "0"
+                BugPercentage = (simulation.BugPercentage.ToString("N0")) + "%" ?? "0"
             });
         }
         return resultTableModels;
@@ -53,14 +58,17 @@ public partial class ListSimulationsResultTable
 
 public class SimulationsResultTableModel
 {
+    public int Id { get; set; }
     public string SimulationName { get; set; }
     public string SimulationDate { get; set; }
     public string TargetDate { get; set; }
     public int Days { get; set; }
     public string Budget { get; set; }
     public string SimCost { get; set; }
+    public string CostDifference { get; set; }
     public string ActualCost { get; set; }
     public string CostPerDay { get; set; }
+    public string SimCostPerDay { get; set; }
     public string DelayPerDay { get; set; }
     public int Epics { get; set; }
     public string BugPercentage { get; set; }
@@ -72,14 +80,17 @@ public class SimulationsResultTableModel
         {
             list.Add(new SimulationsResultTableModel
             {
+                Id = i,
                 SimulationName = "None",
                 SimulationDate = "None",
                 TargetDate = "None",
                 Days = 0,
                 Budget = "0",
                 ActualCost = "0",
+                CostDifference = "0",
                 SimCost = "0",
                 CostPerDay = "0",
+                SimCostPerDay = "0",
                 DelayPerDay = "0",
                 Epics = 0,
                 BugPercentage = "0"
